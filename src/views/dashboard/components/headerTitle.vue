7fa325f066a04e391b62cad77d488ff5b9d560f6
zhouquan <1327195241@qq.com>
2019-05-08 09:50:53 +0800

    headerTitle.vue

--- src/views/dashboard/components/headerTitle.vue
+++ src/views/dashboard/components/headerTitle.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="header-title-container">
-        <canvas id="can" width="200" height="200"></canvas>
+        <div class="header-title">
+            <span>宁波市急救中心智慧急救平台</span>
+        </div>
     </div>
 </template>
 <script>
@@ -10,38 +12,7 @@ export default {
         return {}
     },
     mounted() {
-        let canvas = document.getElementById('can')
-        let _width = 1000;
-        let _height = 500;
-        // 需要动态的赋值宽度和高度
-        canvas.width = _width;
-        canvas.height = _height
-        if(canvas.getContext) {
-            let ctx = canvas.getContext('2d')
-            // ctx.font = '48px 微软雅黑'
-            // ctx.textBaseline = 'hanging'
-            // ctx.fillStyle = 'red'
-            // ctx.fillText('晓军', 130, 20)
         
-            //绘画曲线
-            ctx.beginPath()  //开始绘制
-            ctx.moveTo(200,200)
-            //ctx.lineTo(200,300)
-            console.log(Math.PI * 20)  //圆周率 * 圆的直径 = 圆的周长
-            //Math.PI 是指圆周率
-            //绘制圆弧的方法 arc()  arcTo()
-            ctx.arcTo(200,250,300,320,20)
-            // 200 250 中转点
-            // 300 320 结束点
-            // 10 圆弧的半径
-
-            ctx.strokeStyle = '#058'
-            ctx.lineWidth = 2
-            ctx.stroke()
-            //ctx.closePath()
-
-            //绘制圆角矩形
-        }
     },
     methods:{
         hArcTo() {
@@ -52,14 +23,27 @@ export default {
 </script>
 <style lang="scss" scoped>
 .header-title-container{
-    position:fixed;
-    top:0;
-    left:0;
-    width:100%;
-    height:500px;
-    text-align:center;
-    background:#fff;
-    //color:#fff;
-    z-index:99;
+    height: 43px;
+    width: 100%;
+    display: block;
+    z-index: 2;
+    position: absolute;
+    .header-title{
+    position: absolute;
+    background-image: url(/static/img/header.2ced04e.png);
+    background-size: 100% 43px;
+    height: 43px;
+    width: 100%;
+    background-repeat: no-repeat;
+    display: block;
+    span{
+        position:fixed;
+        width:100%;
+        text-align:center;
+        color:#02a9ff;
+        line-height:40px;
+    }
 }
+}
+
 </style>
